41199e4eea7c516d93d31449a5170c65e73d7779
Kevin Liu <kevinsliu0@gmail.com>
2024-04-06 14:18:53 -0700

    add procedure memory

--- memory/memory/procedural_memory.py
+++ memory/memory/procedural_memory.py
@@ -1,13 +1,71 @@
-from memory.base_memory import BaseMemory
+# from memory.base_memory import BaseMemory
+import sqlite3
+import json
+import dill
 
 
-class ProceduralMemory(BaseMemory):
+class ProceduralMemory:
     def __init__(self):
+        self.conn = sqlite3.connect("/tmp/mem.db")
+        self.cur = self.conn.cursor()
+        self.cur.execute(
+            "CREATE TABLE IF NOT EXISTS tools(name, function BLOB, description, parameters)"
+        )
         self.functions = []
+        
+
+        # add example methods
+        self.store(
+            (
+                "add_two",
+                lambda x: x + 2,
+                "adds the current weather by 2",
+                [("x", "integer", "variable to add to")],
+            )
+        )
 
     def store(self, kv):
-        key, description = kv
-        self.functions((key, description))
+        """
+        name: str
+        function: python def
+        description: str
+        parameters: (listof (name: str, type: str, description: str))
+        """
+        name, function, description, parameters = kv
+        # self.functions((name, function, description, parameters))
+
+        self.cur.execute(
+            """
+            INSERT INTO tools VALUES (?, ?, ?, ?)
+        """,
+            (name, dill.dumps(function), description, json.dumps(parameters)),
+        )
 
     def retrieve(self, obj):
-        return self.functions
+        tools = self.cur.execute("SELECT * FROM tools")
+        formatted_tools = []
+        for tool in tools:
+            name, function, description, parameters = tool
+            parameters = json.loads(parameters)
+            function = dill.loads(function)
+
+            formatted_tools.append(
+                {
+                    "name": name,
+                    "description": description,
+                    "input_schema": {
+                        "type": "object",
+                        "properties": {
+                            param[0]: {"type": param[1], "description": param[2]}
+                            for param in parameters
+                        },
+                        "required": [param[0] for param in parameters]
+                    },
+                }
+            )
+
+        return formatted_tools
+
+mem = ProceduralMemory()
+
+assert mem.retrieve("")[0]["name"] == "add_two"
